71599898e00ef713822f74669e9a57f7c360ddd5
bleerton <bleertons@gmail.com>
2019-09-19 10:37:14 +0200

    bugs fixes

--- js/script.js
+++ js/script.js
@@ -122,5 +122,31 @@ $(document).ready(function () {
         return false;
     });
 
+    $("#navigation li a").click(function(event) {
+		event.preventDefaults() ;
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+
+    });
+    
+    
+    const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.addClass("fixedNav");
+		}
+		else {
+			body.removeClass("fixedNav");
+		}
+    }
 
 });
